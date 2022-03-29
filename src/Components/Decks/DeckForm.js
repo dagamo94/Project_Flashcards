c592ab19757021c0d083561cf7fa679cd2ad5a46
dan <da.ga.mo.94@gmail.com>
2022-03-29 09:00:23 -0600

    create DeckForm comp

---
+++ src/Components/Decks/DeckForm.js
@@ -0,0 +1,10 @@
+// REUSABLE FORM COMPONENT FOR CREATE AND EDIT DECK COMPONENTS
+import React from "react";
+
+export default function DeckForm(){
+    return (
+        <form>
+            
+        </form>
+    )
+}
---
+++ src/Components/Decks/EditDeck.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function EditDeck(){
+    // DECLARE STATE
+
+    // DECLARE HANDLER FUNCTIONS
+
+    // RETURN DECKFORM COMPONENT
+}
