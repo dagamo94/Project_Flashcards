d75b8b2690ae3c432f09e95b56e82533deede872
dan <da.ga.mo.94@gmail.com>
2022-05-26 21:47:41 -0600

    prep for deployment

--- src/utils/api/index.js
+++ src/utils/api/index.js
@@ -2,7 +2,7 @@
  * Defines the base URL for the API.
  * The default values is overridden by the `API_BASE_URL` environment variable.
  */
-const API_BASE_URL = process.env.API_BASE_URL || "http://localhost:8080";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 
 /**
  * Defines the default headers for these functions to work with `json-server`
@@ -31,7 +31,7 @@ function stripCards(deck) {
  * This function is NOT exported because it is not needed outside of this file.
  *
  * @param url
- *  the url for the requst.
+ *  the url for the request.
  * @param options
  *  any options for fetch
  * @param onCancel
